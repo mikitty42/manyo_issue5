3c1ea7f05d7ad8c897ad201148a54a9a8417e394
mikitakezawa <mikiaaamiki@yahoo.co.jp>
2024-02-14 14:14:22 +0900

    Wrote tests for system spec and model spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,12 +9,14 @@ RSpec.describe 'タスクモデル機能', type: :model do
     end
     context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
-        # ここに内容を記載する
+          task = Task.new(title: '失敗テスト', content: '')
+          expect(task).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        # ここに内容を記載する
+          task = Task.new(title: '成功テスト', content: '成功テスト')
+          expect(task).to be_valid
       end
     end
   end
