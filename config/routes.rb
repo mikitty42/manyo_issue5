97729d862772473a661db537d11887f8b19ab7e2
mikitakezawa <mikiaaamiki@yahoo.co.jp>
2024-03-01 21:40:47 +0900

    Implementation of user registration and login functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+    resources :sessions, only: [:new,:create,:destroy]
+    resources :users, only: [:new,:create,:show]
     root to: 'tasks#index'
   resources :tasks do
       collection do
