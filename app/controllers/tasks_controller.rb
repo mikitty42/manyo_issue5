ae55a3d0bdd48f68a43e0e247e6203ca1900e2dd
mikitakezawa <mikiaaamiki@yahoo.co.jp>
2024-03-09 21:26:02 +0900

    Access restrictions for login processing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,75 +1,82 @@
 class TasksController < ApplicationController
-  def index
-      @tasks = Task.page(params[:page]).per(10).order('created_at DESC')
-      if params[:sort_expired]
-          @tasks = Task.page(params[:page]).per(10).order(deadline: 'DESC')
-      else params[:sort_priority]
-          @tasks = Task.page(params[:page]).per(10).order('priority ASC')
-      end
-      if params[:task].present?
-          if params[:task][:title].present? && params[:task][:status].present?
-              @tasks = @tasks.get_by_title(params[:task][:title]).get_by_status(params[:task][:status])
-          elsif params[:task][:title].present?
-              @tasks = @tasks.get_by_title(params[:task][:title])
-          elsif params[:task][:status].present?
-              @tasks = @tasks.get_by_status(params[:task][:status])
-          end
-      end
-  end
-
-  def new
-      @task = Task.new
-  end
-  
-  def create
-      @task = Task.new(task_params)
-      if params[:back]
-          render :new
-      else
-          if @task.save
-              flash[:notice] = 'Taskの投稿に成功しました'
-              redirect_to tasks_path
-          else
-              flash.now[:alert] = 'Taskの投稿に失敗しました'
-              render :new
-          end
-      end
-  end
-
-  def show
-      @task = Task.find(params[:id])
-  end
-
-  def edit
-      @task = Task.find(params[:id])
-  end
-  
-  def update
-      @task = Task.find(params[:id])
-      if @task.update(task_params)
-          flash[:notice] = 'Taskを編集しました'
-          redirect_to tasks_path
-      else
-          flash.now[:alert] = 'Taskの編集に失敗しました'
-          render :edit
-      end
-  end
-  
-  def destroy
-      @task = Task.find(params[:id])
-      @task.destroy
-      flash[:danger] = 'Taskを削除しました'
-      redirect_to tasks_path
-  end
-  
-  def confirm
-      @task = Task.new(task_params)
-      render :new if @task.invalid?
-  end
-  
-  private
-  
-  def task_params
-      params.require(:task).permit(:title,:content,:deadline,:status,:priority)
-  end
+    before_action :authenticate_user, only: [:index]
+    #before_action :correct_user,   only: [:show]
+    def index
+        @tasks = Task.page(params[:page]).per(10).order('created_at DESC')
+        if params[:sort_expired]
+            @tasks = Task.page(params[:page]).per(10).order(deadline: 'DESC')
+        else params[:sort_priority]
+            @tasks = Task.page(params[:page]).per(10).order('priority ASC')
+        end
+        if params[:task].present?
+            if params[:task][:title].present? && params[:task][:status].present?
+                @tasks = @tasks.get_by_title(params[:task][:title]).get_by_status(params[:task][:status])
+            elsif params[:task][:title].present?
+                @tasks = @tasks.get_by_title(params[:task][:title])
+            elsif params[:task][:status].present?
+                @tasks = @tasks.get_by_status(params[:task][:status])
+            end
+            end
+        end
+        
+        def new
+            @task = Task.new
+        end
+        
+        def create
+            @task = current_user.tasks.build(task_params)
+            if params[:back]
+                render :new
+            else
+                if @task.save
+                    flash[:notice] = 'Taskの投稿に成功しました'
+                    redirect_to tasks_path
+                else
+                    flash.now[:alert] = 'Taskの投稿に失敗しました'
+                    render :new
+                end
+            end
+        end
+        
+        def show
+            @task = Task.find(params[:id])
+        end
+        
+        def edit
+            @task = Task.find(params[:id])
+        end
+        
+        def update
+            @task = Task.find(params[:id])
+            if @task.update(task_params)
+                flash[:notice] = 'Taskを編集しました'
+                redirect_to tasks_path
+            else
+                flash.now[:alert] = 'Taskの編集に失敗しました'
+                render :edit
+            end
+        end
+        
+        def destroy
+            @task = Task.find(params[:id])
+            @task.destroy
+            flash[:danger] = 'Taskを削除しました'
+            redirect_to tasks_path
+        end
+        
+        def confirm
+            @task = current_user.tasks.build(task_params)
+            render :new if @task.invalid?
+        end
+        
+        private
+        
+        def task_params
+            params.require(:task).permit(:title,:content,:deadline,:status,:priority)
+        end
+        
+        def correct_user
+            @user = User.find(params[:id])
+            redirect_to root_path unless current_user?(@user)
+        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_28_131818) do
+ActiveRecord::Schema.define(version: 2024_03_03_123825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2024_02_28_131818) do
     t.date "deadline", default: "2024-02-24", null: false
     t.integer "status"
     t.integer "priority"
+    t.bigint "user_id", null: false
     t.index ["title"], name: "index_tasks_on_title"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2024_02_28_131818) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
