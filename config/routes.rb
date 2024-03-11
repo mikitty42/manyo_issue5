4772f7ffba901b200e7400038d9dece4cd7c43fc
mikitakezawa <mikiaaamiki@yahoo.co.jp>
2024-03-11 15:16:38 +0900

    管理画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
     resources :sessions, only: [:new,:create,:destroy]
-    resources :users, only: [:new,:create,:show]
     root to: 'tasks#index'
   resources :tasks do
       collection do
           post :confirm
     end
+  end
+  namespace :admin do
+  resources :users
   end
 end
